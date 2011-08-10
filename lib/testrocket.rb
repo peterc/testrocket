44c1b649b88b6ffe48817bae58a38512d8daecd4
Peter Cooper <git@peterc.org>
2011-08-10 23:19:09 +0100

    Added IO handle support for output

--- lib/testrocket.rb
+++ lib/testrocket.rb
@@ -1,13 +1,15 @@
 module TestRocket
+  module Out; attr_accessor :out; end; extend Out
+  
   def _test(a, b)
     send((call rescue()) ? a : b)
   end
 
-  def +@; puts _test :_pass, :_fail; end
-  def -@; puts _test :_fail, :_pass; end
+  def +@; r = _test :_pass, :_fail; (TestRocket.out || $>).puts r; r end
+  def -@; r = _test :_fail, :_pass; (TestRocket.out || $>).puts r; r end
 
-  def _pass; '   OK'; end
-  def _fail; " FAIL @ #{source_location.join(':')}"; end
+  def _pass; '  OK'; end
+  def _fail; "FAIL @ #{source_location.join(':')}"; end
 end
 
 Proc.send :include, TestRocket
