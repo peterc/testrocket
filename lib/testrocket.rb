b94b0103d6b3f41118a50ce095721259388c493d
Peter Cooper <git@peterc.org>
2011-08-14 01:02:45 +0100

    Changed from puts to << to support Logger instances

--- lib/testrocket.rb
+++ lib/testrocket.rb
@@ -5,12 +5,12 @@ module TestRocket
     send((call rescue()) ? a : b)
   end
 
-  def +@; r = _test :_pass, :_fail; (TestRocket.out || $>).puts r; r end
-  def -@; r = _test :_fail, :_pass; (TestRocket.out || $>).puts r; r end
-  def ~@; r = _pend;                (TestRocket.out || $>).puts r; r end
-  def !@; r = _desc;                (TestRocket.out || $>).puts r; r end
+  def +@; r = _test :_pass, :_fail; (TestRocket.out || $>) << r << "\n"; r end
+  def -@; r = _test :_fail, :_pass; (TestRocket.out || $>) << r << "\n"; r end
+  def ~@; r = _pend;                (TestRocket.out || $>) << r << "\n"; r end
+  def !@; r = _desc;                (TestRocket.out || $>) << r << "\n"; r end
   
-  def _pass; '     OK'; end
+  def _pass; "     OK"; end
   def _fail; "   FAIL @ #{source_location.join(':')}"; end
   def _pend; "PENDING '#{call.to_s}' @ #{source_location.join(':')}"; end
   def _desc; "   FIRE '#{call.to_s}'!"; end
